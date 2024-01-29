b0fe59e95bdffbdf737f62244c11e066333fe059
larrymutoni <larrycarnot@gamil.com>
2024-01-29 18:46:08 +0300

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by Larry Mutoni on 12/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
